182c0972e1116401e7d727fbf5d966360ca6641a
Chaichan <chaichanchaisak2549@gmail.com>
2024-12-27 16:30:47 +0700

    lab13_1.cpp completed

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,33 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+
+
+void stat(const double A[],int N ,double B[])
+{
+    double sum = 0 , Geo = 1, Har = 0 , sum2 = 0 , max = A[0], min = A[0];
+    
+    for(int i = 0 ; i < N ; i++){
+        sum += A[i];
+        Geo *= A[i];
+        Har += 1/A[i];
+    }
+    
+    
+    for(int i = 0 ; i < N ; i++){
+        sum2 += pow((A[i] - (sum/N)) , 2);
+    }
+
+    for(int i = 1 ; i < N ; i++){
+        if(A[i] > max) max = A[i];
+        if(A[i] < min) min = A[i];
+        
+    }
+    
+    B[0] = sum/N;
+    B[1] = sqrt(sum2/(N));
+    B[2] = pow(Geo , (1.0/N));
+    B[3] = N/Har;
+    B[4] = max;
+    B[5] = min;
+}
